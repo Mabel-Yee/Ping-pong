e12ecafb8a1d1870d274a351fe9b9003a9e6221a
Mabel-Yee <119719062+Mabel-Yee@users.noreply.github.com>
2022-12-17 12:11:16 +0800

    Update files

---
+++ PING-PONG.py
@@ -0,0 +1,85 @@
+from pygame import *
+
+class GameSprite(sprite.Sprite):
+    #class constructor
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        super().__init__() #sprite.Sprite.__init__(self)
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+ 
+        #every sprite must have the rect property – the rectangle it is fitted in
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    #method drawing the character on the window
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+#child class
+class Paddle (GameSprite):
+    #method to control the sprite with arrow keys
+    def update_left(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 1:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_width - 5:
+            self.rect.y += self.speed
+    def update_right(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 1:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_width - 5:
+            self.rect.y += self.speed
+
+#interface
+BLUE = (200, 255, 255)
+win_width = 700
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(BLUE)
+
+#create sprites (paddle and balls)
+paddleA_img = "paddle_blue.png"
+paddleB_img = "paddle_red.png"
+ball_img = "tennis_ball.png"
+
+paddleLeft = Paddle (paddleA_img, 20, 200, 30, 150, 150)
+paddleRight = Paddle (paddleB_img, 650, 200, 30, 150, 150)
+ball = GameSprite(ball_img, 330, 200, 50, 50, 50)
+
+
+#game loop
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+#fonts
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+
+speed_x = 3
+speed_y = 3
+
+while game:
+    for e in event.get():
+        if e.type ==QUIT:
+            game = False
+
+    if finish != True:
+        window.fill (BLUE)
+        paddleLeft.update_right()
+        paddleRight.update_left()
+
+        ball.rect.x += speed_x
+        ball.rect.x += speed_y
+
+        paddleLeft.reset()
+        paddleRight.reset()
+        ball.reset()
+
+    display.update()
+    clock.tick(FPS)
